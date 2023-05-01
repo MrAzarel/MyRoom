9f1fae9e10303925a7229866fc226313fbe27c36
MrAzarel <arseniy.mazurenko.03@gmail.com>
2023-05-02 01:12:24 +0300

    Добавьте файлы проекта.

---
+++ MyRoom/IPolygon.cs
@@ -0,0 +1,14 @@
+﻿namespace Interfaces
+{
+    public interface IPolygon
+    {
+        public int ID { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public decimal[] Center { get; }
+        public decimal[,] Vertices { get; }
+
+        public void Move(decimal centerDeltaX, decimal centerDeltaY);
+        public void Rotate(int angle);
+    }
+}
